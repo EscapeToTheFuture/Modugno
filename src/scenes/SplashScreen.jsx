686be3edcd7861fa0eebd7e9ea048b85b7aa4b3c
T_Ferrarah <55454285+TFerrarah@users.noreply.github.com>
2025-04-19 15:25:15 +0200

    Title Screen

--- src/scenes/SplashScreen.jsx
+++ src/scenes/SplashScreen.jsx
@@ -1,9 +1,66 @@
-import React from 'react';
+/* eslint-disable react/prop-types */
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+import Button from "@components/Button";
 
-const SplashScreen = () => {
+import backgroundImage from "@assets/images/Scena5/Il parco comunale.webp";
+
+const SplashScreen = ({ title, location }) => {
+  const [hasRequestedFullscreen, setHasRequestedFullscreen] = useState(false);
+
+
+  const navigate = useNavigate();
+  // reset localstorage
+  localStorage.clear();
+
+  useEffect(() => {
+    if (hasRequestedFullscreen) return;
+
+    const handleFullscreenChange = () => {
+      if (document.fullscreenElement) {
+        setHasRequestedFullscreen(true);
+      }
+    };
+
+    document.addEventListener("fullscreenchange", handleFullscreenChange);
+    
+    return () => {
+      document.removeEventListener("fullscreenchange", handleFullscreenChange);
+    };
+
+
+  }, [hasRequestedFullscreen]);
+  
+
   return (
-    <div className='bg-white h-screen w-screen flex items-center justify-center'>
-      <h1>Escape Room Modugno</h1>
+    <div
+      className="w-full flex flex-col items-center justify-center gap-2 md:gap-10 h-svh bg-center bg-clip-border bg-cover bg-origin-border bg-no-repeat gap-auto"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    >
+      <div className="absolute inset-0 bg-black opacity-60"></div>
+
+      <h1 className="2xl:w-1/2 md:w-2/3 w-2/3 text-5xl md:text-5xl xl:text-8xl font-bold text-center select-none font-elite text-cyan-400 z-1">
+        {title}
+      </h1>
+      <div className="flex items-end justify-center gap-1">
+        <h3 className="font-elite text-3xl md:text-3xl xl:text-6xl z-1 text-white select-none">
+          {location}
+        </h3>
+      </div>
+      <Button
+        onClick={async () => {
+          if (!hasRequestedFullscreen) {
+            try {
+              await document.body.requestFullscreen();
+            } catch (err) {
+              console.error(err.name, err.message);
+            }
+          }
+          navigate("/scena1");
+        }}
+      >
+        INIZIA
+      </Button>
     </div>
   );
 };
