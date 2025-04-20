4e5a8a34cc78def679e1892d93a7a53168993e06
T_Ferrarah <55454285+TFerrarah@users.noreply.github.com>
2025-04-20 09:46:35 +0200

    Aggiornato favicon e og-image

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-d9a5ed57'], (function (workbox) { 'use strict';
     "revision": "d7f6f8fceb9c15d0786df320ec78b747"
   }, {
     "url": "index.html",
-    "revision": "0.6uiq7tk31e"
+    "revision": "0.triroldh2m"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
